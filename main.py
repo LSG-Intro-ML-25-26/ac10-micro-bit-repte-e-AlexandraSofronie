a1ae8abcfae87a2cb4ac50a9086bd39a9e2773eb
Alexandra Sofronie <alexandrateodora.sofronie@gracia.lasalle.cat>
2025-12-09 05:13:44 +0100

    final commit

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
+mostrar_grafico = True
+x = 2
+y = 2
 
+def on_forever():
+    global mostrar_grafico
+    if input.button_is_pressed(Button.A):
+        mostrar_grafico = True
+    elif input.button_is_pressed(Button.B):
+        mostrar_grafico = False
+    if  mostrar_grafico:
+        grafico()
+    else:
+        show_drop()
+
+def grafico():
+    led.plot_bar_graph(input.temperature(), 50)
+
+def show_drop():
+    global x, y
+    accX = input.acceleration(Dimension.X)
+    accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(100)
+    basic.clear_screen()
+
+    if accX <= 150 and x > 0:
+        x -= 1
+    if accX > 150 and x < 4:
+        x += 1
+    if accY <= 150 and y > 0:
+        y -= 1
+    if accY > 150 and y < 4:
+        y += 1
+
+while True:
+    on_forever()
--- main.ts
+++ main.ts
@@ -1,1 +1,51 @@
+let mostrar_grafico = true
+let x = 2
+let y = 2
+function on_forever() {
+    
+    if (input.buttonIsPressed(Button.A)) {
+        mostrar_grafico = true
+    } else if (input.buttonIsPressed(Button.B)) {
+        mostrar_grafico = false
+    }
+    
+    if (mostrar_grafico) {
+        grafico()
+    } else {
+        show_drop()
+    }
+    
+}
 
+function grafico() {
+    led.plotBarGraph(input.temperature(), 50)
+}
+
+function show_drop() {
+    
+    let accX = input.acceleration(Dimension.X)
+    let accY = input.acceleration(Dimension.Y)
+    led.plot(x, y)
+    basic.pause(100)
+    basic.clearScreen()
+    if (accX <= 150 && x > 0) {
+        x -= 1
+    }
+    
+    if (accX > 150 && x < 4) {
+        x += 1
+    }
+    
+    if (accY <= 150 && y > 0) {
+        y -= 1
+    }
+    
+    if (accY > 150 && y < 4) {
+        y += 1
+    }
+    
+}
+
+while (true) {
+    on_forever()
+}
